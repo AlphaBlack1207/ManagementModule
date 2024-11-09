e2ad28b9be17077f53f548587790eeff9489b1da
AlphaBlack1207 <michelalejandro0107@gmail.com>
2024-11-08 21:01:46 -0500

    Agregar estilos CSS para jKanban y configurar el controlador de tableros con datos de ejemplo

--- app/javascript/controllers/board_controller.js
+++ app/javascript/controllers/board_controller.js
@@ -2,37 +2,88 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   connect() {
+    const board = [
+      {
+        id: "board-id-1",
+        title: "Board Title 1",
+        class: "bg-blue-500, shadow-lg, rounded-lg",
+        item: [
+          {
+            id: "item-id-1",
+            title: "Item 1",
+            class: ["rounded-lg", "font-medium", "text-sm"],
+            username: "username1",
+          },
+          {
+            id: "item-id-2",
+            title: "Item 2",
+            username: "username2",
+          },
+        ],
+      },
+      {
+        id: "board-id-2",
+        title: "Board Title 2",
+        item: [
+          {
+            id: "item-id-1",
+            title: "Item 1",
+            username: "username1",
+          },
+          {
+            id: "item-id-2",
+            title: "Item 2",
+            username: "username2",
+          },
+        ],
+      },
+      {
+        id: "board-id-3",
+        title: "Board Title 3",
+        item: [
+          {
+            id: "item-id-1",
+            title: "Item 1",
+            username: "username1",
+          },
+          {
+            id: "item-id-2",
+            title: "Item 2",
+            username: "username2",
+          },
+        ],
+      },
+    ];
     var kanban = new jKanban({
-      element: "#board", // selector of the kanban container
-      gutter: "15px", // gutter of the board
-      widthBoard: "250px", // width of the board
-      responsivePercentage: false, // if it is true I use percentage in the width of the boards and it is not necessary gutter and widthBoard
-      dragItems: true, // if false, all items are not draggable
-      boards: [], // json of boards
-      dragBoards: true, // the boards are draggable, if false only item can be dragged
+      element: "#board",
+      gutter: "15px",
+      widthBoard: "250px",
+      responsivePercentage: false,
+      dragItems: true,
+      boards: board,
+      dragBoards: true,
       itemAddOptions: {
-        enabled: false, // add a button to board for easy item creation
-        content: "+", // text or html content of the board button
-        class: "kanban-title-button btn btn-default btn-xs", // default class of the button
-        footer: false, // position the button on footer
+        enabled: false,
+        content: "+",
+        class: "kanban-title-button btn btn-default btn-xs",
+        footer: false,
       },
       itemHandleOptions: {
-        enabled: false, // if board item handle is enabled or not
-        handleClass: "item_handle", // css class for your custom item handle
-        customCssHandler: "drag_handler", // when customHandler is undefined, jKanban will use this property to set main handler class
-        customCssIconHandler: "drag_handler_icon", // when customHandler is undefined, jKanban will use this property to set main icon handler class. If you want, you can use font icon libraries here
-        customHandler: "<span class='item_handle'>+</span> %title% ", // your entirely customized handler. Use %title% to position item title
-        // any key's value included in item collection can be replaced with %key%
+        enabled: false,
+        handleClass: "item_handle",
+        customCssHandler: "drag_handler",
+        customCssIconHandler: "drag_handler_icon",
+        customHandler: "<span class='item_handle'>+</span> %title% ",
       },
-      click: function (el) {}, // callback when any board's item are clicked
-      context: function (el, event) {}, // callback when any board's item are right clicked
-      dragEl: function (el, source) {}, // callback when any board's item are dragged
-      dragendEl: function (el) {}, // callback when any board's item stop drag
-      dropEl: function (el, target, source, sibling) {}, // callback when any board's item drop in a board
-      dragBoard: function (el, source) {}, // callback when any board stop drag
-      dragendBoard: function (el) {}, // callback when any board stop drag
-      buttonClick: function (el, boardId) {}, // callback when the board's button is clicked
-      propagationHandlers: [], // the specified callback does not cancel the browser event. possible values: "click", "context"
+      click: function (el) {},
+      context: function (el, event) {},
+      dragEl: function (el, source) {},
+      dragendEl: function (el) {},
+      dropEl: function (el, target, source, sibling) {},
+      dragBoard: function (el, source) {},
+      dragendBoard: function (el) {},
+      buttonClick: function (el, boardId) {},
+      propagationHandlers: [],
     });
     console.log("kanban", kanban);
   }
